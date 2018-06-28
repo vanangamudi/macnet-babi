e50cb7d7486fc34a3e74e7d944288b809b270285
M Selvakumar <selva.developer@gmail.com>
2018-06-28 17:56:20 +0530

    macnet: qi generation takes into account of memory

---
+++ macnet_mod3/hpconfig.py
@@ -0,0 +1,18 @@
+import logging
+
+class ConfigMeta(type):
+    def __getattr__(cls, attr):
+        return cls._default
+
+class Base(metaclass=ConfigMeta):
+    pass
+
+class CONFIG(Base):
+    hidden_size = 50
+    embed_size = 50
+    batch_size = 128
+
+    reasoning_steps = 2
+
+    LR = 0.001
+    MOMENTUM=0.1
