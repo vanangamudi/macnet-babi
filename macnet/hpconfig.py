0131e6deb89980b709c969753c7fd4c12f99c358
M Selvakumar <selva.developer@gmail.com>
2018-08-07 00:42:03 +0530

    hpconfig.py contains tasks list - configures which tasks to learn

--- macnet/hpconfig.py
+++ macnet/hpconfig.py
@@ -8,12 +8,35 @@ class Base(metaclass=ConfigMeta):
     pass
 
 class CONFIG(Base):
+    tasks = [
+        1,    # Single supporting act
+        2,    # two supporting facts
+        3,    # three supporting facts
+        4,    # two arg relations
+        5,    # three arg relations 
+        6,    # yes-no-questions
+        7,    # counting
+        8,    # list-sets
+        9,    # simple-negation
+        10,   # indefinite-knowledge
+        11,   # basic-coreference
+        12,   # conjuction
+        13,   # compound-coreference
+        14,   # time-reasoning
+        15,   # basic-deduction
+        16,   # basic-induction
+        17,   # positional-reasoning
+        18,   # size-reasoning
+        19,   # path-finding
+        20,   # agent-motivations
+    ] 
+
     trainset_size = 1.0
     hidden_size = 20
     embed_size = 20
     num_layers = 1
-    reasoning_steps = 2
+    reasoning_steps = 4
     
     LR = 0.001
     MOMENTUM=0.1
-    ACTIVATION = 'sigmoid'
+    ACTIVATION = 'softmax'
