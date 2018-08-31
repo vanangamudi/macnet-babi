6c062632604af7a92f02ba3df8f1ac46b768242c
Mu Selvakumar <selva.developer@gmail.com>
2018-08-31 23:02:06 +0530

    one more minor change

--- macnet/expt.sh
+++ macnet/expt.sh
@@ -15,17 +15,17 @@ time python main.py --hpconfig hpconfig_30percent_dataset.py  train  && echo $(d
 time python main.py --hpconfig hpconfig_40percent_dataset.py  train  && echo $(date)
 time python main.py --hpconfig hpconfig_50percent_dataset.py  train  && echo $(date)
 time python main.py --hpconfig hpconfig_75percent_dataset.py  train  && echo $(date)
-								     && echo $(date)
+
 time python main.py --hpconfig hpconfig_1_reasoning_steps.py  train  && echo $(date)
 time python main.py --hpconfig hpconfig_3_reasoning_steps.py  train  && echo $(date)
 time python main.py --hpconfig hpconfig_4_reasoning_steps.py  train  && echo $(date)
 time python main.py --hpconfig hpconfig_5_reasoning_steps.py  train  && echo $(date)
-								     && echo $(date)
+
 time python main.py --hpconfig hpconfig_no_graph_reasoning.py train  && echo $(date)
 time python main.py --hpconfig hpconfig_no_prev_mem.py	      train  && echo $(date)
 time python main.py --hpconfig hpconfig_no_same_rnn.py	      train  && echo $(date)
 time python main.py --hpconfig hpconfig_no_story_again.py     train  && echo $(date)
-								     && echo $(date)
+
 time python main.py --hpconfig hpconfig_story_len_10.py	      train  && echo $(date)
 time python main.py --hpconfig hpconfig_story_len_20.py	      train  && echo $(date)
 time python main.py --hpconfig hpconfig_story_len_30.py	      train  && echo $(date)
