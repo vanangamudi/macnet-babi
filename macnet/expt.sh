3ed0e6d5493e7d32b7fc16fca1f66f910b8465f4
Mu Selvakumar <selva.developer@gmail.com>
2018-09-03 22:28:16 +0530

    can use more gpus for different tasks

--- macnet/expt.sh
+++ macnet/expt.sh
@@ -1,35 +1,58 @@
 #!/bin/sh
 
-echo $(date)
-time python main.py --hpconfig hpconfig.py  train
-
-for i in $(seq 1 20)
-do
+function normal {
     echo $(date)
-    time python main.py --hpconfig hpconfig$i.py  train && echo $(date)
-done
+    time python main.py --hpconfig hpconfig.py  train
 
-time python main.py --hpconfig hpconfig_10percent_dataset.py  train  && echo $(date)
-time python main.py --hpconfig hpconfig_20percent_dataset.py  train  && echo $(date)
-time python main.py --hpconfig hpconfig_30percent_dataset.py  train  && echo $(date)
-time python main.py --hpconfig hpconfig_40percent_dataset.py  train  && echo $(date)
-time python main.py --hpconfig hpconfig_50percent_dataset.py  train  && echo $(date)
-time python main.py --hpconfig hpconfig_75percent_dataset.py  train  && echo $(date)
+    for i in $(seq 1 20)
+    do
+	echo $(date)
+	time python main.py --hpconfig hpconfig$i.py  train && echo $(date)
+    done
+}
 
-time python main.py --hpconfig hpconfig_1_reasoning_steps.py  train  && echo $(date)
-time python main.py --hpconfig hpconfig_3_reasoning_steps.py  train  && echo $(date)
-time python main.py --hpconfig hpconfig_4_reasoning_steps.py  train  && echo $(date)
-time python main.py --hpconfig hpconfig_5_reasoning_steps.py  train  && echo $(date)
+function dataset_subset {
 
-time python main.py --hpconfig hpconfig_no_graph_reasoning.py train  && echo $(date)
-time python main.py --hpconfig hpconfig_no_prev_mem.py	      train  && echo $(date)
-time python main.py --hpconfig hpconfig_no_same_rnn.py	      train  && echo $(date)
-time python main.py --hpconfig hpconfig_no_story_again.py     train  && echo $(date)
+    time python main.py --hpconfig hpconfig_10percent_dataset.py  train  && echo $(date)
+    time python main.py --hpconfig hpconfig_20percent_dataset.py  train  && echo $(date)
+    time python main.py --hpconfig hpconfig_30percent_dataset.py  train  && echo $(date)
+    time python main.py --hpconfig hpconfig_40percent_dataset.py  train  && echo $(date)
+    time python main.py --hpconfig hpconfig_50percent_dataset.py  train  && echo $(date)
+    time python main.py --hpconfig hpconfig_75percent_dataset.py  train  && echo $(date)
 
-time python main.py --hpconfig hpconfig_story_len_10.py	      train  && echo $(date)
-time python main.py --hpconfig hpconfig_story_len_20.py	      train  && echo $(date)
-time python main.py --hpconfig hpconfig_story_len_30.py	      train  && echo $(date)
-time python main.py --hpconfig hpconfig_story_len_40.py	      train  && echo $(date)
-time python main.py --hpconfig hpconfig_story_len_50.py	      train  && echo $(date)
-time python main.py --hpconfig hpconfig_story_len_60.py	      train  && echo $(date)
+}
 
+function reasoning_steps {
+    time python main.py --hpconfig hpconfig_1_reasoning_steps.py  train  && echo $(date)
+    time python main.py --hpconfig hpconfig_3_reasoning_steps.py  train  && echo $(date)
+    time python main.py --hpconfig hpconfig_4_reasoning_steps.py  train  && echo $(date)
+    time python main.py --hpconfig hpconfig_5_reasoning_steps.py  train  && echo $(date)
+}
+
+function component_variations {
+    time python main.py --hpconfig hpconfig_no_graph_reasoning.py train  && echo $(date)
+    time python main.py --hpconfig hpconfig_no_prev_mem.py	      train  && echo $(date)
+    time python main.py --hpconfig hpconfig_no_same_rnn.py	      train  && echo $(date)
+    time python main.py --hpconfig hpconfig_no_story_again.py     train  && echo $(date)
+}
+
+function story_len {
+    time python main.py --hpconfig hpconfig_story_len_10.py	      train  && echo $(date)
+    time python main.py --hpconfig hpconfig_story_len_20.py	      train  && echo $(date)
+    time python main.py --hpconfig hpconfig_story_len_30.py	      train  && echo $(date)
+    time python main.py --hpconfig hpconfig_story_len_40.py	      train  && echo $(date)
+    time python main.py --hpconfig hpconfig_story_len_50.py	      train  && echo $(date)
+    time python main.py --hpconfig hpconfig_story_len_60.py	      train  && echo $(date)
+
+}
+
+
+export CUDA_VISIBLE_DEVICES=0
+dataset_subset &
+
+export CUDA_VISIBLE_DEVICES=1
+reasoning_steps&
+
+export CUDA_VISIBLE_DEVICES=2
+normal
+story_len
