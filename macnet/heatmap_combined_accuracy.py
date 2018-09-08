d7ad1eab98246fff405d0a82197ea778f72472e6
M Selvakumar <selva.developer@gmail.com>
2018-09-08 19:19:26 +0530

    code refactor to avoid duplication

--- macnet/heatmap_combined_accuracy.py
+++ macnet/heatmap_combined_accuracy.py
@@ -8,6 +8,7 @@ from anikattu.utilz import initialize_task
 import pickle
 import glob
 import numpy as np
+
 task_names = {
     1:'single-supporting-fact',
     2:'two-supporting-facts',
@@ -58,111 +59,124 @@ colors = {
     21: '#000000',
 }
 
-accuracies = {}
-max_epoch_count = 0
-min_epoch_count = 10000000
 epoch_limit = 400
+    
+def read_pkls():
+    
+    accuracies = {}
+    max_epoch_count = 0
+    min_epoch_count = 10000000
+    
+    hpconfig = 'hpconfig'
+    HPCONFIG = importlib.__import__(hpconfig)
+    tasks = '-'.join(str(i) for i in HPCONFIG.CONFIG.tasks)
+    root_dir= initialize_task(hpconfig + '.py')
+    print('root_dir: {}'.format(root_dir))
+    for filename in glob.glob('{}/results/metrics/*.accuracy.pkl'.format(root_dir)):
+        try:
+            task = os.path.basename(filename).split('.')[0]
+            accuracies[task] = pickle.load(
+                open(filename,
+                     'rb')
+            )
 
-hpconfig = 'hpconfig'
-HPCONFIG = importlib.__import__(hpconfig)
-tasks = '-'.join(str(i) for i in HPCONFIG.CONFIG.tasks)
-root_dir= initialize_task(hpconfig + '.py')
-print('root_dir: {}'.format(root_dir))
-for filename in glob.glob('{}/results/metrics/*.accuracy.pkl'.format(root_dir)):
-    try:
-        task = os.path.basename(filename).split('.')[0]
-        accuracies[task] = pickle.load(
-            open(filename,
-                 'rb')
-        )
-
-        if len(accuracies[task]) > 51:
-            accuracies[task][50] = accuracies[task][49]
-            accuracies[task][51] = accuracies[task][50]
-        if len(accuracies[task]) > 101:
-            accuracies[task][100] = accuracies[task][99]
-            accuracies[task][101] = accuracies[task][100]
-        if len(accuracies[task]) > 151:
-            accuracies[task][150] = accuracies[task][149]
-            accuracies[task][151] = accuracies[task][150]
-
-        if len(accuracies[task]) < min_epoch_count:
-            min_epoch_count = len(accuracies[task])
-            print('min_epoch_count: {}'.format(min_epoch_count))
+            if len(accuracies[task]) < min_epoch_count:
+                min_epoch_count = len(accuracies[task])
+                print('min_epoch_count: {}'.format(min_epoch_count))
 
 
-        if len(accuracies[task]) > max_epoch_count:
-            max_epoch_count = len(accuracies[task])
-            print('max_epoch_count: {}'.format(max_epoch_count))
-    except:
-        print('{} not found'.format(filename))
-right_most_boundary = min(epoch_limit, min_epoch_count)
-fig, ax = plt.subplots(1, 1, figsize=(max(10, right_most_boundary//10), 5))
+            if len(accuracies[task]) > max_epoch_count:
+                max_epoch_count = len(accuracies[task])
+                print('max_epoch_count: {}'.format(max_epoch_count))
+        except:
+            print('{} not found'.format(filename))
 
-# Remove the plot frame lines. They are unnecessary here.
-ax.spines['top'].set_visible(False)
-ax.spines['bottom'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['left'].set_visible(False)
+    return accuracies, min_epoch_count, max_epoch_count
 
-# Ensure that the axis ticks only show up on the bottom and left of the plot.
-# Ticks on the right and top of the plot are generally unnecessary.
-ax.get_xaxis().tick_bottom()
-ax.get_yaxis().tick_left()
 
-#fig.subplots_adjust(left=.06, right=.75, bottom=.02, top=.94)
-fig.subplots_adjust(left=.1, bottom=.02)
-# Limit the range of the plot to only where the data is.
-# Avoid unnecessary whitespace.
 
-ax.set_xlim(0, right_most_boundary)
-#ax.set_ylim(0.98, 1.01)
+def plot_accuracies(epoch_limit,
+                    min_epoch_count, max_epoch_count,
+                    accuracies, task_ids,
+                    plot_title='Combined Accuracy',
+                    plot_filepath='combined_accuracy_heatmap.png',
+):
+    right_most_boundary = min(epoch_limit, min_epoch_count)
+    fig, ax = plt.subplots(1, 1, figsize=(max(10, right_most_boundary//10), 5))
 
-# Make sure your axis ticks are large enough to be easily read.
-# You don't want your viewers squinting to read your plot.
-#plt.xticks(range(1970, 2011, 10), fontsize=14)
-#plt.yticks(range(0, 91, 10), fontsize=14)
-#ax.xaxis.set_major_formatter(plt.FuncFormatter('{:.0f}'.format))
-#ax.yaxis.set_major_formatter(plt.FuncFormatter('{:.0f}%'.format))
+    # Remove the plot frame lines. They are unnecessary here.
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
 
-# Provide tick lines across the plot to help your viewers trace along
-# the axis ticks. Make sure that the lines are light and small so they
-# don't obscure the primary data lines.
-#plt.grid(True, 'major', 'y', ls='--', lw=.5, c='k', alpha=.3)
+    # Ensure that the axis ticks only show up on the bottom and left of the plot.
+    # Ticks on the right and top of the plot are generally unnecessary.
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
 
-# Remove the tick marks; they are unnecessary with the tick lines we just
-# plotted.
-#plt.tick_params(axis='both', which='both', bottom='off', top='off',
-#                labelbottom='on', left='off', right='off', labelleft='on')
+    #fig.subplots_adjust(left=.06, right=.75, bottom=.02, top=.94)
+    fig.subplots_adjust(left=.1, bottom=.02)
+    # Limit the range of the plot to only where the data is.
+    # Avoid unnecessary whitespace.
 
-labels = []
-matrix = []
-accuracies_ = sorted(accuracies.items(), key=lambda x: task_ids[x[0]])
-for i, (task_name, acc) in  enumerate(accuracies_):
-    labels.append('{}({:0.2f}/{:0.2f})'.format(task_name, max(acc), acc[right_most_boundary-1]))
-    matrix.append(acc[:right_most_boundary])
+    ax.set_xlim(0, right_most_boundary)
+    #ax.set_ylim(0.98, 1.01)
 
-plt.sca(ax)
+    # Make sure your axis ticks are large enough to be easily read.
+    # You don't want your viewers squinting to read your plot.
+    #plt.xticks(range(1970, 2011, 10), fontsize=14)
+    #plt.yticks(range(0, 91, 10), fontsize=14)
+    #ax.xaxis.set_major_formatter(plt.FuncFormatter('{:.0f}'.format))
+    #ax.yaxis.set_major_formatter(plt.FuncFormatter('{:.0f}%'.format))
 
-matrix = np.asarray(matrix)
-aspect_ratio = max(1, matrix.shape[1]/matrix.shape[0])
-print(matrix.shape, aspect_ratio)
-im = ax.imshow(np.asarray(matrix, dtype=np.float), clim=(0,1), cmap='autumn',
-               aspect=aspect_ratio)
-print(labels)
-plt.yticks(range(len(labels)),labels)
+    # Provide tick lines across the plot to help your viewers trace along
+    # the axis ticks. Make sure that the lines are light and small so they
+    # don't obscure the primary data lines.
+    #plt.grid(True, 'major', 'y', ls='--', lw=.5, c='k', alpha=.3)
 
-fig.colorbar(im, orientation='vertical', aspect=aspect_ratio)
+    # Remove the tick marks; they are unnecessary with the tick lines we just
+    # plotted.
+    #plt.tick_params(axis='both', which='both', bottom='off', top='off',
+    #                labelbottom='on', left='off', right='off', labelleft='on')
 
-# Make the title big enough so it spans the entire plot, but don't make it
-# so big that it requires two lines to show.
+    labels = []
+    matrix = []
+    for i, (task_name, acc) in  enumerate(accuracies_):
+        labels.append('{}({:0.2f}/{:0.2f})'.format(task_name, max(acc), acc[right_most_boundary-1]))
+        matrix.append(acc[:right_most_boundary])
 
-# Note that if the title is descriptive enough, it is unnecessary to include
-# axis labels; they are self-evident, in this plot's case.
-fig.suptitle('Combined Accuracy', fontsize=18, ha='center')
+    plt.sca(ax)
 
-# Finally, save the figure as a PNG.
-# You can also save it as a PDF, JPEG, etc.
-# Just change the file extension in this call.
-plt.savefig('combined_accuracy_heatmap.png', bbox_inches='tight')
-plt.show()
+    matrix = np.asarray(matrix)
+    aspect_ratio = max(1, matrix.shape[1]/matrix.shape[0])
+    print(matrix.shape, aspect_ratio)
+    im = ax.imshow(np.asarray(matrix, dtype=np.float), clim=(0,1), cmap='autumn',
+                   aspect=aspect_ratio)
+    print(labels)
+    plt.yticks(range(len(labels)),labels)
+
+    fig.colorbar(im, orientation='vertical', aspect=aspect_ratio)
+
+    # Make the title big enough so it spans the entire plot, but don't make it
+    # so big that it requires two lines to show.
+
+    # Note that if the title is descriptive enough, it is unnecessary to include
+    # axis labels; they are self-evident, in this plot's case.
+    fig.suptitle(plot_title, fontsize=18, ha='center')
+
+    # Finally, save the figure as a PNG.
+    # You can also save it as a PDF, JPEG, etc.
+    # Just change the file extension in this call.
+    plt.savefig(plot_filepath, bbox_inches='tight')
+    plt.show()
+
+
+if __name__ == '__main__':
+    accuracies, min_epoch_count, max_epoch_count = read_pkls()
+    accuracies = sorted(accuracies.items(), key=lambda x: task_ids[x[0]])
+    plot_accuracies(epoch_limit,
+                    min_epoch_count, max_epoch_count,
+                    accuracies, task_ids,
+                    'Combined Accuracy',
+                    'combined_accuracy_heatmap.png')
